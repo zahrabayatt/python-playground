af7f9c302c09c869e4fd0f11364f437aaf138cd9
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-24 01:14:15 +0330

    Python Extensions

--- app.py
+++ app.py
@@ -1,14 +1,2 @@
-# When it comes to typing Python code, you have two options:
-# code editor like  VS code, Atom, and Sublime
-# IDE like Pycharm
-
-# to run the code, run this command: 
-# python [filename]  
-# example: python app.py
-
-print('Hello world 😺'); # print on the console
-print("*" * 10); # print 10 times star
-
-
-
-
+# python extension : providing access points for extensions to seamlessly integrate and offer support for IntelliSense (Pylance), debugging (Python Debugger), formatting, linting, code navigation, refactoring, variable explorer, test explorer, and more!
+# autopep8 extension : formatter according to python pep 8 which is style guid for python code https://peps.python.org/pep-0008/
