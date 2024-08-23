928cd4e26d20fa80f5b091663585ccb41bd6f6ed
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-24 01:14:43 +0330

    For Loops

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-# python extension : providing access points for extensions to seamlessly integrate and offer support for IntelliSense (Pylance), debugging (Python Debugger), formatting, linting, code navigation, refactoring, variable explorer, test explorer, and more!
-# autopep8 extension : formatter according to python pep 8 which is style guid for python code https://peps.python.org/pep-0008/
+for number in range(3):
+    print("Attempt", number + 1, (number + 1) * ".")
+
+for number in range(1, 4):
+    print("Attempt", number, number * ".")
+
+for number in range(1, 10, 2):
+    print("Attempt", number + 1, (number + 1) * ".")
