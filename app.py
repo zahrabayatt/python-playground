8e3883618703b8a2d4799eaedc74d4f5b2c6ff08
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-24 00:24:01 +0330

    What is Python?

---
+++ app.py
@@ -0,0 +1,15 @@
+# what is python? 
+# It's is the world's fastest growing and most popular programming language not just amongst software developers, but also amongst mathematicians data analysts, scientists, accountants, network engineers and even kids.
+
+# what makes Python so special? 
+# With Python you can solve complex problems in less time with fewer lines of code then many other languages.  Python is a multipurpose language and you can use it for a wide range of jobs such as data analysis, AI/ML, writing automation, scripts. Building web, mobile, and desktop applications,as well as software testing or even hacking.
+# It's cross platform, which means you can build and run Python apps on Windows, Mac, and Linux, it has a huge community, so whenever you get stuck there is someone out there to help.
+# it has a large ecosystem of libraries, frameworks, and tools. Whatever you want to do, it is likely that someone else done it before. Because Python has been around for over 20 years.
+
+# There are two versions of Python out there. Python2 which is the legacy version of Python. And is going to be supported until year 2020. And Python 3 which is Python for the future.
+
+
+
+
+
+
