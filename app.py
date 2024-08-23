ce960abee300c81c1e94e29831253df12705b436
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-08-24 00:31:06 +0330

    Python Interpreter

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-# To download python head over to https://www.python.org/
 
-# If you're on Windows, you're going to see add Python path check box in installer app, make sure to check that.
+# when you type this command in cmd you see the environment that called python interpreter:
+# python
 
-# To verify that you installed Python properly type this command in cmd: python
+# python interpreter is basically a program that executes python code. We can type our python code in a file and give it to this interpreter or we can type our code directly here in this interactive shell.
+
+
