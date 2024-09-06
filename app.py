d4dd8d884e5ad1f7d35390feac7ab57c0b90c1c0
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-06 16:55:01 +0330

    String Methods

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-first = "Zahra"
-last = "Bayat"
-full = first + " " + last
-full = f"{first} {last}"  # prefix could be lowercase  or uppercase f or F
-full = f"{len(first) + 2} {last}"
-print(full)
+# String Methods:
+
+course = "  Python programming  "
+print(course.upper())  # return a new string, original string didn't effected
+print(course.lower())
+print(course.title())
+print(course.strip())  # trim space
+print(course.rstrip())
+print(course.lstrip())
+print(course.find("pro"))
+print(course.find("Pro"))
+print(course.find("p"))
+print(course.replace("p", "j"))
+print("pro" in course)  # check if the string is exist in course
+print("swift" not in course)
+print(course)
