1bd09c86fc1422811c06e420a000484cd0d0606e
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-06 16:39:43 +0330

    Strings

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-# variable Names:
-# - they should be descriptive and meaningful
-# - we should use lowercase letters to name them
-# - we should use an underscore to separate multiple words
-# - we should put a space around this equal sign
+# whenever you work with texts you should surround your text with quotes.
 
-students_count = 1000
-rating = 4.99
+# You can either use double quotes or single quotes. That's more of a personal preference, but quite often we use double quotes.
+
 course_name = "Python Programming"
-is_published = False
+
+# We also have triple quotes and we use them to format a long string. for example if you have, let's say a variable message. That is the message we want to include in the body of an email. You can use triple quotes to format it like this.
+
+message = """
+Hi John, 
+
+This is Zahra from companyName.com
+
+Blah blah blah
+"""
+
+# a few useful things you can do with strings:
+
+# - For getting the length of strings, we have built-in function:
+print(len(course_name))
+
+# - For getting access to a specific character in the string, we use square bracket notation:
+print(course_name[0])  # it returns first character form the start of string
+print(course_name[2])  # it returns third character form the start of string
+print(course_name[-2])  # it returns second character form the end of string
+
+# - For slicing a string, we can use:
+print(course_name[0:3])
+print(course_name[0:])
+print(course_name[:3])
+print(course_name[:])  # it returns a copy of original string
