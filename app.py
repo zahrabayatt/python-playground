f02dc2946ed1827b00be3684adb9d89487ad1c21
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-06 16:02:08 +0330

    Variable Names

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-# Variables: we use variables to store data in computer's memory.
+# variable Names:
+# - they should be descriptive and meaningful
+# - we should use lowercase letters to name them
+# - we should use an underscore to separate multiple words
+# - we should put a space around this equal sign
 
-# when we run this program, Python interpreter will allocate some memory and stor this number(1000) in that memory space. then it will have this variable reference, that memory location. So this variable is just like a label for that memory location. we can use this variable or this label anywhere in our program to get access to that memory location and the data stored there.
 students_count = 1000
-print(students_count)
-
-# What kind of data can we store in computer's memory?
-# - built in primitive types: Primitive types can be numbers, booleans and strings. for example:
-students_count = 2000  # numbers with decimal point
-rating = 4.99  # float or floating point number
-course_name = "Python Programming"  # strings
-is_published = False  # boolean values can be False or True
-
-# Python is a case sensitive language, which means lowercase and uppercase character have different meanings so boolean values should always start with capital letters and false or FALSE is not accepted boolean values in Python.
+rating = 4.99
+course_name = "Python Programming"
+is_published = False
