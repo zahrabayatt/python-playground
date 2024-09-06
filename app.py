c6bbfd25d4d7d54ebffb68a093448b64f95542a8
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-06 16:48:00 +0330

    Formatted Strings

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-# escape : \
-# escape sequences:
-# \"
-# \'
-# \\
-# \n : new line
-
-course = 'Python "Programming"'
-course = "Python \n\'\\\"Programming\""
-print(course)
+first = "Zahra"
+last = "Bayat"
+full = first + " " + last
+full = f"{first} {last}"  # prefix could be lowercase  or uppercase f or F
+full = f"{len(first) + 2} {last}"
+print(full)
