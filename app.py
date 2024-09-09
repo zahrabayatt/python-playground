37f9828005888bdead9934115b7641c04a49218c
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-09 22:41:30 +0330

    Iterables

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 for x in range(5):
-    for y in range(3):
-        print(f"({x}, {y})")
+    print(x)
+
+print(type(range(5)))  # <class 'range'>
+
+# In python we have primitive types and complex types, range is one of the complex types.
+
+# range object is iterable which means you can iterate over it or use it in a for loop.
+
+# range function return a range object that is iterable and because of that we can iterate it using for loop.
+
+# There is other object that is iterable in Python like strings:
+
+for x in "Python":
+    print(x)
+
+# we have another complex type called list which we use it to store a list of objects, this is also iterable:
+
+for x in [1, 2, 3, 4]:
+    print(x)
+
+# custom objects are iterable.
