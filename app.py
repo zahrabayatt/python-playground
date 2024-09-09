16b2cb24d0bd5c4cd5e72bf7a44d8c03dbb2990a
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-09 22:33:48 +0330

    Nested Loops

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-successful = True
-# successful = False
-for number in range(3):
-    print("Attempt", number, number * ".")
-    if successful:
-        print("Successful")
-        break
-else:  # Executed when the for never breaks and complete iteration
-    print("Attempted 3 times and fail")
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
