6071d44a36c719479a6711d719c545204d2fb1b8
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-09 22:22:15 +0330

    Quiz

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-# age should be between 18 and 65
-age = 22
-if age >= 18 and age < 65:
-    print("Eligible")
-# or
-if 18 <= age < 65:  # This is what we called Chaining Comparison Operators
-    print("Eligible")
+# What is output?
+if 10 == "10":
+    print("a")
+elif "bag" > "apple" and "bag" > "cat":
+    print("b")
+else:
+    print("c")
+# output: c
