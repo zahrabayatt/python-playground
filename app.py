9b0ffe6ee276e18968542b082eec238a8a16917c
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-09 22:51:46 +0330

    Infinite Loops

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-# So you have learned that we use for loops to iterate over iterable objects.
+# command = ""
+# while command.lower() != "quit":
+#     command = input(">")
+#     print("ECHO", command)
 
-# In python We have another kind of loop that is a while loop:
-
-number = 100
-while number > 0:
-    print(number)
-    number //= 2  # or number = number // 2
-
-# Example : simulate interactive python shell
-command = ""
-while command.lower() != "quit":
+while True:
     command = input(">")
     print("ECHO", command)
+    if command.lower() == "quit":
+        break
