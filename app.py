464e614a3e189bdd4c2893b02d2b2e801af04c25
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-10 00:10:26 +0330

    Arguments

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-def greet():
-    print("Hi there")
+def greet(first_name, last_name):  # parameters
+    print(f"Hi {first_name} {last_name}")
     print("Welcome aboard")
 
 
-greet()
-# pep8: add two line after function
+greet("Zahra", "Bayat")  # arguments
+greet("John", "Smith")
+# greet() by default all parameters are required!
