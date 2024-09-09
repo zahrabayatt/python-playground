08fb4ef0db703dc1f808dbf73818852db09fcd5f
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-09 22:09:29 +0330

    Logical Operators

--- app.py
+++ app.py
@@ -1,3 +1,29 @@
-age = 24
-message = "Eligible" if age >= 18 else "Not eligible"
-print(message)
+# Logical Operators:
+# and
+# or
+# not
+
+high_income = True
+good_credit = True
+student = True
+
+if high_income and good_credit:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+if high_income or good_credit:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+if not student:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+
+if (high_income or good_credit) and not student:
+    print("Eligible")
+else:
+    print("Not eligible")
