155a3683b1668ed5539885959cfd245a5a7ea5f7
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-09 22:04:25 +0330

    Ternary Operator

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-temperature = 15
-if temperature > 30:
-    print("It's warm")
-    print("Drink water")
-elif temperature > 20:
-    print("It's nice")
-else:
-    print("It's cold")
-print("Done")
+age = 24
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
