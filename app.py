b84713787665bb43e8af25bf968e659e4b124488
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-09 22:31:12 +0330

    For..Else

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+successful = True
+# successful = False
 for number in range(3):
-    print("Attempt", number + 1, (number+1) * ".")
-
-for number in range(1, 4):
     print("Attempt", number, number * ".")
-
-for number in range(1, 10, 2):
-    print("Attempt", number, number * ".")
+    if successful:
+        print("Successful")
+        break
+else:  # Executed when the for never breaks and complete iteration
+    print("Attempted 3 times and fail")
