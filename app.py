9447e64601b5b62cf62d31bb5d246b7bb5a7e3fe
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-20 20:17:46 +0330

    Arrays

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-x = 10
-y = 11
+# if you dealing with large of items in the list, we have a more efficient type in python called Array
 
-# write code to swap the value of variables:
+from array import array
 
-z = x
-x = y
-y = z
+# array function get a typecode as argument which is string of one character that determines the type of object in the array:
+# typecode in python3 docs: https://docs.python.org/3/library/array.html
 
-# another way:
-x, y = y, x  # we're basically defining a tuple and unpacking it
+numbers = array('i', [1, 2, 3])
 
-print("x", x)
-print("y", y)
+# array like list has these methods and we can access the item by its index:
+numbers.append(4)
+numbers.insert(2, 9)
+numbers.pop()
+numbers.remove(9)
+print(numbers[0])
+
+# we can not add item with different type in array unlike lists
+numbers[0] = 1.0  # we got type error
