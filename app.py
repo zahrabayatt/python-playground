970737b375005af37accab6a87df728f36b80a16
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-20 22:58:54 +0330

    Handling Different Exceptions

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-# it's our job as developer to prevent happening exceptions in the code and crashing program through these exceptions
-
 try:
     age = int(input("Age: "))
+    xfactor = 10/age
 except ValueError as ex:
     print("You didn't enter a valid age.")
-    print(ex)
-    print(type(ex))
+except ZeroDivisionError:
+    print("Age can not be zero.")
 else:
     print("No exceptions were thrown.")
 print("Execution continues.")
+
+
+try:
+    age = int(input("Age: "))
+    xfactor = 10/age
+except (ValueError, ZeroDivisionError) as ex:
+    print("You didn't enter a valid age.")
+    print(ex)
+except ZeroDivisionError:
+    print("Age can not be zero.")  # it is not going to be executed!
+else:
+    print("No exceptions were thrown.")
+print("Execution continues.")
+
+# if we have multiple except, and one of them executed, other ones are going to be ignored.
