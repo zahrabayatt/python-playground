f6b2a0d60e5bc6e3fb9bda34c7045e79e4a8aa64
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-20 18:15:34 +0330

    Map Function

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ items = [
     ("product3", 12),
 ]
 
-
-def sort_item(item):
-    return item[1]
-
-# items.sort(key=sort_item)
-
-
-# another way is use lambda functions:
-items.sort(key=lambda item: item[1])
+prices = []
+for item in items:
+    prices.append(item[1])
 
 print(items)
+print(prices)
+
+# better way is using map function:
+
+map_object = map(lambda item: item[1], items)
+
+prices = list(map(lambda item: item[1], items))
+
+print(map_object)
+print(prices)
