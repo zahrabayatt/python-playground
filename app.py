f435fe40a8e038514d50739ef722159eb94d3f1c
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-20 23:28:37 +0330

    Raising Exceptions

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
+# python3 built-in exception docs: https://docs.python.org/3/library/exceptions.html
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less.")
+    return 10 / age
+
+
 try:
-    # whenever we open a file using a with statement, python will automatically call file.close. in other words the with statement is used to automatically release external resources
-    # we can open multiple file using the with statement:
-    # with open("app.py") as file, open("another.txt") as target:
-
-    with open("app.py") as file:
-        print("File opened.")
-        # if a object has these two magic methods, it supports the context management protocol. the magic method start with __
-        file.__enter__
-        file.__exit__
-        # if a object supports the context management protocol, we can use as object with the with statement and we don't need finally block.
-
-    age = int(input("Age: "))
-    xfactor = 10/age
-except (ValueError, ZeroDivisionError):
-    print("You didn't enter a valid age.")
-else:
-    print("No exceptions were thrown.")
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)
