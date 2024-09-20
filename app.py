afb5526e1b4195386bd3a90e43518b93b4b96cb5
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-20 20:55:40 +0330

    Set and Dictionary Comprehensions

--- app.py
+++ app.py
@@ -1,44 +1,19 @@
-# in dictionary in python,we use immutable types for keys, so quiet often we use string or number for keys, but for values we can use any type.
+values = []
 
-point = {"x": 1, "y": 2}
+for x in range(5):
+    values.append(x * 2)
 
-# we can use dict function for defining dictionary:
-point = dict(x=1, y=2)
+# better solution:
 
-# we can access items by key not the index:
-point["x"] = 10
-print(point["x"])
+values = [x * 2 for x in range(5)]
+print(values)
 
-# add new item to dictionary:
-point["z"] = 20
+# we can also use comprehensions for set and dictionary:
 
-print(point)
+# we use curly braces to define dictionary and set:
 
-# if we use the key that didn't exist we got error:
-# print(point["a"])
+values = {x * 2 for x in range(5)}
+print(values)
 
-# to check if key exist:
-if "a" in point:
-    print(point["a"])
-
-# Or we can use get method to get value by key, if key didn't exist it returns None but we can also set the default value if key didn't exist:
-print(point.get("a"))
-print(point.get("a", 0))
-
-# to delete item in dictionary:
-del point["x"]
-
-print(point)
-
-# to loop over a dictionary:
-
-for key in point:
-    print(key, point[key])
-
-# or
-
-for x in point.items():
-    print(x)
-
-for key, value in point.items():
-    print(key, value)
+values = {x: x * 2 for x in range(5)}
+print(values)
