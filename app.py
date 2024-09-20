8b5087cf0535b703aef9f1d1e7bf6f85b49ba038
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-20 18:02:34 +0330

    Finding Items in the List

--- app.py
+++ app.py
@@ -1,35 +1,16 @@
 letters = ["a", "b", "c"]
 
-# Adding Items in Lists:
+print(letters.index("c"))
 
-# Add item to end of the list
-letters.append("d")
+# if item doesn't exist in the list we got the error
+# print(letters.index("d"))
 
-# Add item in specific position in the List
-letters.insert(2, "-")
+# to prevent this error we should first check the item is exist and then find it in the list:
+if "d" in letters:
+    print(letters.index("d"))
 
-# Add item in the begining of the list
-letters.insert(0, "h")
+# to get numbers of the item with specific value:
+print(letters.count("d"))
 
-
-# Remove items in lists:
-
-# Remove item at the end of the list
-letters.pop()
-# you can pass the index to remove a item in given index
-letters.pop(0)
-
-# remove a first item with specific value
-letters.remove("-")
-# for remove all items with specific value you should iterate the letter and use remove method
-
-# another way to delete item in specific position
-del letters[0]
-
-# delete a range of items in the list
-del letters[0:2]
-
-# to remove all items in the list
-letters.clear()
-
-print(letters)
+# we must to pass a argument to count method unless we got the error
+# print(letters.count())
