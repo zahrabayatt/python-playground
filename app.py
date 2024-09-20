809320fb6c7ec0396a99fbc3bcab72d1c018fb1a
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-20 23:18:16 +0330

    The With Statement

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 try:
-    file = open("app.py")
-    try:
-        age = int(input("Age: "))
-        xfactor = 10/age
-        # file.close()
-    except (ValueError, ZeroDivisionError):
-        print("You didn't enter a valid age.")
-    except Exception:
-        print("Something went wrong...")
-    else:
-        print("No exceptions were thrown.")
-    finally:
-        file.close()
-except Exception:
-    print("Something went wrong:")
+    # whenever we open a file using a with statement, python will automatically call file.close. in other words the with statement is used to automatically release external resources
+    # we can open multiple file using the with statement:
+    # with open("app.py") as file, open("another.txt") as target:
 
-print("Execution continues.")
+    with open("app.py") as file:
+        print("File opened.")
+        # if a object has these two magic methods, it supports the context management protocol. the magic method start with __
+        file.__enter__
+        file.__exit__
+        # if a object supports the context management protocol, we can use as object with the with statement and we don't need finally block.
+
+    age = int(input("Age: "))
+    xfactor = 10/age
+except (ValueError, ZeroDivisionError):
+    print("You didn't enter a valid age.")
+else:
+    print("No exceptions were thrown.")
