aea17fd2eea47d007490577df650f43af4aa98ad
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-20 18:11:24 +0330

    Lambda Functions

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
-numbers = [3, 51, 2, 8, 6]
-
-# sort acc
-numbers.sort()
-
-# sort dec
-numbers.sort(reverse=True)
-
-# to get the new list with specific sort:
-print(sorted(numbers, reverse=True))
-print(sorted(numbers))
-
-print(numbers)
-
 items = [
     ("product1", 10),
     ("product2", 9),
     ("product3", 12),
 ]
 
-# for sorting lists with complex object like list of topples, the sort method didn't work
-items.sort()
-print(items)
-
-# to sort list with complex object:
-
 
 def sort_item(item):
-    return item[1]  # sort items base on price
+    return item[1]
+
+# items.sort(key=sort_item)
 
 
-items.sort(key=sort_item)
+# another way is use lambda functions:
+items.sort(key=lambda item: item[1])
+
 print(items)
