eb3846d2a17de8c7b432d7ef54f8a913f70e5338
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-20 17:37:56 +0330

    Looping over Lists

--- app.py
+++ app.py
@@ -1,34 +1,14 @@
-numbers = [1, 2, 3]
-first = numbers[0]
-second = numbers[1]
-third = numbers[2]
+letters = ["a", "b", "c"]
+for letter in letters:
+    print(letter)
 
-# another way is to unpack list like that:
-first, second, third = numbers
+# to get index and value of each element in list:
 
-# the number of variables that we have on the left side of the assignment operator should be equal  to the number of items we have in the list.
-# first, second = numbers # we got the error
+for letter in enumerate(letters):
+    print(letter, letter[0], letter[1])
 
-# we unpack only the first two element of list and pack other element in another variable:
-first, second, *other = numbers
-print(first)
-print(second)
-print(other)
+# The enumerate object yields pairs containing a index (from start, which defaults to zero) and a value yielded by the iterable argument.
 
-# when we prefix a parameter with an asterisk, Python would get all these arbitrary argument and packed them into a list:
-
-
-def multiply(*numbers):
-    total = 1
-    for number in numbers:
-        total *= number
-    return total
-
-
-multiply(1, 2, 3, 4, 5, 6)
-
-# if we want only first and last item in the list:
-first, *other, last = numbers
-print(first)
-print(last)
-print(other)
+# unpack the topple:
+for index, letter in enumerate(letters):
+    print(index, letter)
