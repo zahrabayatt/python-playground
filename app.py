adb6c3c1edc8aac0d0c7a1f25b6d85acabfa0d99
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-20 22:53:01 +0330

    Handling Exceptions

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-from pprint import pprint
+# it's our job as developer to prevent happening exceptions in the code and crashing program through these exceptions
 
-# Exercise: what character is most repeated in this sentence?
-
-sentence = "This is a common interview question"
-
-char_frequency = {}
-for char in sentence:
-    if char in char_frequency:
-        char_frequency[char] += 1
-    else:
-        char_frequency[char] = 1
-
-# pprint(char_frequency, width=1)
-
-char_frequency_sorted = sorted(
-    char_frequency.items(),
-    key=lambda kv: kv[1],
-    reverse=True)
-
-print(char_frequency_sorted[0])
+try:
+    age = int(input("Age: "))
+except ValueError as ex:
+    print("You didn't enter a valid age.")
+    print(ex)
+    print(type(ex))
+else:
+    print("No exceptions were thrown.")
+print("Execution continues.")
