b89a46b9d37a5108bec618a5d02a387c1e6894d2
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-25 21:16:11 +0330

    Magic Methods

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 class Point:
-
+    # __init__ is a magic method.
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    # class method:
-    @classmethod  # decorator
-    def zero(cls):  # cls is a reference to class
-        return cls(0, 0)
+    # we can reimplement the str method:
+    def __str__(self):
+        return f"({self.x}, {self.y})"
 
-    # instance method
     def draw(self):
         print(f"Point ({self.x}, {self.y})")
 
 
 point = Point(1, 2)
-point.draw()
 
-another = Point.zero()
-another.draw()
+# magic methods wrap two underline and they call automatically by python interpreter
+# magic docs: https://realpython.com/python-magic-methods/#representing-objects-as-strings
+
+print(point)
+print(point.__str__())
+print(str(point))
+# by default it returns the name of module and the memory object followed by memory address.
+# <__main__.Point object at 0x00000205ECFF7FD0>
