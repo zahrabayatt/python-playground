4866fef788c9f89d375c9032cc21965d23febd3d
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-25 20:56:02 +0330

    Class vs Instance Attributes

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 
 class Point:
-    # every functions that we define in class must has at least one parameter which we called by convention self.
-    # self is a reference to current object.
+    # class attribute
+    default_color = "red"
+
     def __init__(self, x, y):
-        # x, y is attributes of Point
         self.x = x
         self.y = y
 
@@ -12,8 +12,20 @@ class Point:
 
 
 point = Point(1, 2)
-print(point.x)
-print(point.y)
-
-# we dont need to supply the value for self argument, python interpreter does that for us.
 point.draw()
+
+# we can also define attributes after defending object because objects in python like JS is dynamic
+point.z = 10
+
+# all the attributes that we are defining so far(x,y,z), are instance attributes, so every point object has attributes with different value
+
+another = Point(3, 4)
+another.draw()
+
+
+# we can also define class attributes and these are attributes that we define in class level and they are shared with all instances.
+Point.default_color = 'yellow'
+
+print(point.default_color)
+print(Point.default_color)
+print(another.default_color)
