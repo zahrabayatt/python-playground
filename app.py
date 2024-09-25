157acf63a3b7c17e9534f302ab53f90a4915d247
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-25 21:01:43 +0330

    Class vs Instance Methods

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-
 class Point:
-    # class attribute
-    default_color = "red"
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    # class method:
+    @classmethod  # decorator
+    def zero(cls):  # cls is a reference to class
+        return cls(0, 0)
+
+    # instance method
     def draw(self):
         print(f"Point ({self.x}, {self.y})")
 
@@ -14,18 +17,5 @@ class Point:
 point = Point(1, 2)
 point.draw()
 
-# we can also define attributes after defending object because objects in python like JS is dynamic
-point.z = 10
-
-# all the attributes that we are defining so far(x,y,z), are instance attributes, so every point object has attributes with different value
-
-another = Point(3, 4)
+another = Point.zero()
 another.draw()
-
-
-# we can also define class attributes and these are attributes that we define in class level and they are shared with all instances.
-Point.default_color = 'yellow'
-
-print(point.default_color)
-print(Point.default_color)
-print(another.default_color)
