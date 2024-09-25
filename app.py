4f562ba981a402460073bbbe4f5382d7d0c6eb01
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-25 20:31:34 +0330

    Creating Classes

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-# Class: it is a blueprint for creating new objects.
-# Object: it is a instance of a class.
+# we use all lowercase letters and separate multiple words by underscore for variable and function names.
+# we use pascal naming convention for classes.
 
-# Class: Human
-# Object: John, Mary, Jack
+class Point:
+    def draw(self):  # all functions in classes must have at least one parameter.
+        print("draw")
 
-x = 1
-print(type(x))
-# <class 'int'> - in python we have this class to create integer.
+
+point = Point()
+print(type(point))
+# output: <class '__main__.Point'> - main is the name of our module.
+point.draw()
+
+# to check if the object is instance of class:
+print(isinstance(point, Point))
+print(isinstance(point, int))
