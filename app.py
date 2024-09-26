4b22e903d7eafe76ae3f7869737874f1e0f87e65
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-27 00:35:54 +0330

    Making Custom Containers

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
 
-    def __add__(self, other):
-        return Point(self.x+other.x, self.y + other.y)
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+
+    # it tasks two arguments: self, key
+    def __getitem__(self, tag):
+        return self.tags.get(tag.lower(), 0)
+
+    # it tasks three arguments: self, key, value
+    def __setitem__(self, tag, count):
+        self.tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __iter__(self):
+        # iter is a built in function to get an iterator from an object.
+        return iter(self.tags)
 
 
-point = Point(1, 2)
-other = Point(1, 2)
+cloud = TagCloud()
+cloud.add("python")
+cloud.add("python")
+cloud.add("python")
+print(cloud.tags)
 
-combined = point + other
-print(combined)
-print(combined.x, combined.y)
+cloud.add("Python")
+print(cloud.tags)
 
+# reimplement __getitem__ magic method to unable to read count of tags like this:
+print(cloud["python"])
 
-# other Arithmetic Operations docs: https://docs.python.org/3/reference/datamodel.html#emulating-numeric-types
+# reimplement __getitem__ magic method to unable to set count of tags like this:
+cloud["python"] = 2
+print(cloud.tags)
+
+# reimplement __len__ magic method to unable to get numbers of tags like this:
+print(len(cloud))
+
+# reimplement __iter__ magic method to make it iterable, so we can iterate over it using a for loop:
+for tag in cloud:
+    print(tag)
