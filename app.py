dc08cad0fa23dc1fffd4bcad424f2befbd41cd74
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-27 19:36:08 +0330

    Multi-level Inheritance

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# too much of every thing is a bad thing! and that is very true about inheritance.
 class Animal:
     def __init__(self):
         print("Animal Constructor")
@@ -7,26 +8,15 @@ class Animal:
         print("eat")
 
 
-class Mammal(Animal):
-    # we overwriting the base constructor and we don't have age attribute
-    # def __init__(self):
-    #     self.weight = 2
-
-    def __init__(self):
-        super().__init__()  # we call base constructor then we have age attribute
-        print("Mammal Constructor")
-        self.weight = 2
-        # we can change the order of calling constructor by moving super().__init__() to end of the mammal constructor
-
-    def walk(self):
-        print("walk")
+class Brid(Animal):
+    def fly(self):
+        print("fly")
 
 
-class Fish(Animal):
-    def swim(self):
-        print("swim")
+# but chechen can not fly!
+class Chicken(Brid):
+    pass
 
-
-m = Mammal()
-print(m.age)
-print(m.weight)
+# Another example of inheritance abuse is the concept of employees. People say, "an employee is a person, a person is a living creature," and so on. This is what we call multilevel inheritance, and it can really complicate your software.
+# Just because an employee is a person and a person is a living creature doesn’t mean you should have these classes in your software. Your methods should solve a business problem. For example, just because an animal can eat in real life doesn’t mean your animal class needs an eat method.
+# Avoid multilevel inheritance. If you use inheritance, keep it to one or two levels. Trust me, going beyond that? You're going to shoot yourself in the foot.
