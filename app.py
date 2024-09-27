8eef511e16c53f780d1ffc724483c42c79a875ff
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-09-27 20:21:16 +0330

    Multiple Inheritance

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
-# too much of every thing is a bad thing! and that is very true about inheritance.
-class Animal:
-    def __init__(self):
-        print("Animal Constructor")
-        self.age = 1
-
-    def eat(self):
-        print("eat")
+# In Python a class can have multiple base classes.
+# similar to multilevel inheritance it's a source of issues If you don't use it properly, you're going to introduce all sorts of bugs in your programs.
+class Employee:
+    def great(self):
+        print("Employee Greet")
 
 
-class Brid(Animal):
-    def fly(self):
-        print("fly")
+class Person:
+    def great(self):
+        print("Person Great")
 
 
-# but chechen can not fly!
-class Chicken(Brid):
+class Manager(Employee, Person):
     pass
 
-# Another example of inheritance abuse is the concept of employees. People say, "an employee is a person, a person is a living creature," and so on. This is what we call multilevel inheritance, and it can really complicate your software.
-# Just because an employee is a person and a person is a living creature doesn’t mean you should have these classes in your software. Your methods should solve a business problem. For example, just because an animal can eat in real life doesn’t mean your animal class needs an eat method.
-# Avoid multilevel inheritance. If you use inheritance, keep it to one or two levels. Trust me, going beyond that? You're going to shoot yourself in the foot.
+
+manager = Manager()
+manager.great()  # Employee greet
+# We get employee greet because we added the employee class first. So when the Python interpreter tries to execute this line, first it looks at the manager class to see if it has a method called greet. If it doesn't, it will look at its first base class. Does the employee have a greet method? Yes. So, the lookup terminates here. If the employee class doesn't have the greet method, then it will look at the person class.
+
+# So why is this an issue? Because if tomorrow another programmer joins our team and, for whatever reason, they decide to change the order of these base classes, our program will have a different behavior. So if they move this person class over here and run the program, now we get a different output.
+
+# Multiple inheritance is not always a bad thing. It's bad if you don't use it properly.
+# Good Example of Using Multiple Inheritance:
+
+
+class Flyer:
+    def fly(self):
+        pass
+
+
+class Swimmer:
+    def swim(self):
+        pass
+
+
+class FlyingFish(Flyer, Swimmer):
+    pass
